e25ebc945cdf7aabacd16f5232c412d7e810da81
Yong Hyun <0203jyh00@naver.com>
2023-09-12 21:43:22 +0900

    MOD : tittleWithBackArrow 함수 위치 offset으로 바꿈

--- app/src/main/java/com/example/usw_random_chat/ui/Widjet.kt
+++ app/src/main/java/com/example/usw_random_chat/ui/Widjet.kt
@@ -3,13 +3,11 @@ package com.example.usw_random_chat.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,10 +17,10 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -87,13 +85,14 @@ fun tittleWithBackArrow(text: String, modifier: Modifier) {
         Modifier, //horizontalArrangement = Arrangement.Center
     )
     {
-        Spacer(Modifier.width(screenWidthInDp.dp))
+        //Spacer(Modifier.width(screenWidthInDp.dp))
         IconButton(onClick = { /*TODO*/ }) {
             Icon(
                 imageVector = Icons.Filled.ArrowBack, contentDescription = "",
                 Modifier
                     .height(36.dp)
                     .width(36.dp)
+                    .offset(x=screenWidthInDp.dp)
             )
         }
         Text(
