039718ed4e7e71368e563d25d4e46c7df2516f6b
Yong Hyun <0203jyh00@naver.com>
2023-11-12 21:45:12 +0900

    merge : 풀받음

---
+++ app/src/main/java/com/example/usw_random_chat/data/repositoryimpl/ProfileRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.example.usw_random_chat.data.repositoryimpl
+
+import com.example.usw_random_chat.data.api.ProfileApiService
+import com.example.usw_random_chat.data.dto.ProfileDTO
+import com.example.usw_random_chat.data.repository.ProfileRepository
+import javax.inject.Inject
+
+class ProfileRepositoryImpl @Inject constructor(private val profileApiService: ProfileApiService) : ProfileRepository {
+
+    override suspend fun setProfile(param: ProfileDTO): ProfileDTO {
+        val response = profileApiService.setProfile(param)
+
+        if (response.isSuccessful){
+            return response.body()!!
+        }else{
+            throw Exception("Fail!!")
+        }
+    }
+
+}
