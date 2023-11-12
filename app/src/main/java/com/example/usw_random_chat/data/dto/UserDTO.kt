f009b8ed006e8f4e44362cb7ec75a70225daf386
Yong Hyun <0203jyh00@naver.com>
2023-11-12 18:41:56 +0900

    feat :  변수명 수정

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,10 +3,12 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
+    @SerializedName("memberID")
+    val memberID : String = "",
+    @SerializedName("memberPW")
+    val memberPW : String = "",
     @SerializedName("memberEmail")
-    val memberEmail : String = "",
-    @SerializedName("memberPassword")
-    val memberPassword : String = "",
-    @SerializedName("memberName")
-    val memberName : String = ""
+    val memberEmail : String = ""
 )
+
+
