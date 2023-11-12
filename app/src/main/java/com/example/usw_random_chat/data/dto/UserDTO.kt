ee05335b8f2d907ffb8b8c8561234e5ae2d8a4f6
Yong Hyun <0203jyh00@naver.com>
2023-11-12 21:51:19 +0900

    feat : signup에서 서버와 주고받을 테이블 컬럼값

--- app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
+++ app/src/main/java/com/example/usw_random_chat/data/dto/UserDTO.kt
@@ -3,12 +3,10 @@ package com.example.usw_random_chat.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class UserDTO(
-    @SerializedName("memberID")
-    val memberID : String = "",
-    @SerializedName("memberPW")
-    val memberPW : String = "",
     @SerializedName("memberEmail")
-    val memberEmail : String = ""
+    val memberEmail : String = "",
+    @SerializedName("memberPW")
+    val memberPassword : String = "",
+    @SerializedName("memberID")
+    val memberID : String = ""
 )
-
-
