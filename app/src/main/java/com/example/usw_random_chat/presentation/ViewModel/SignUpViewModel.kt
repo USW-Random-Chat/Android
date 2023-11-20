62f4b7ba4ad4e73fc662b4a71489c6f1392c5fe1
Yong Hyun <0203jyh00@naver.com>
2023-11-20 22:45:24 +0900

    feat :import com.example.usw_random_chat.domain.usecase.ProfileUseCase 라 되어있어 import com.example.usw_random_chat.domain.usecase.RegisterUseCase로 수정

--- app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignUpViewModel.kt
+++ app/src/main/java/com/example/usw_random_chat/presentation/ViewModel/SignUpViewModel.kt
@@ -3,17 +3,20 @@ package com.example.usw_random_chat.presentation.ViewModel
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.example.usw_random_chat.domain.usecase.ProfileUseCase
+import com.example.usw_random_chat.domain.usecase.RegisterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import androidx.lifecycle.viewModelScope
+
+
 @HiltViewModel
-class SignUpViewModel @Inject constructor(private val profileUseCase: ProfileUseCase) : ViewModel()  {
+class SignUpViewModel @Inject constructor(private val registerUseCase: RegisterUseCase) : ViewModel()  {
     private val _rememberId  = mutableStateOf("")
     private val _rememberPw  = mutableStateOf("")
     private val _rememberPwCheck  = mutableStateOf("")
     private val _rememberEmail   = mutableStateOf("")
-    private var _rememberPwEqualOrNot = mutableStateOf(false)
-    private var _rememberTrigger = mutableStateOf(false)
+    private val _rememberPwEqualOrNot = mutableStateOf(false)
+    private val _rememberTrigger = mutableStateOf(false)
 
     val rememberId : State<String> = _rememberId
     val rememberPw : State<String>  = _rememberPw
