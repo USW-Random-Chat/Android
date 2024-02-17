461d77ee8b0dc90c344535530d5e84fe46837f18
Yong Hyun <0203jyh00@naver.com>
2024-02-17 18:09:44 +0900

    Mod : VisibleText(눈 아이콘 있는 텍스트필드),TextfiledTitle(텍스트필드 위에 나오는 제목과 빨간글씨)로 변경

--- app/src/main/java/com/example/usw_random_chat/presentation/view/PwChange.kt
+++ app/src/main/java/com/example/usw_random_chat/presentation/view/PwChange.kt
@@ -1,46 +1,26 @@
 package com.example.usw_random_chat.presentation.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.usw_random_chat.R
 import com.example.usw_random_chat.presentation.ViewModel.UserModifyViewModel
-import com.example.usw_random_chat.ui.GetScreenWidthInDp
-import com.example.usw_random_chat.ui.RedWarning
+import com.example.usw_random_chat.ui.TextfiledTitle
+import com.example.usw_random_chat.ui.VisibleText
 import com.example.usw_random_chat.ui.button
 import com.example.usw_random_chat.ui.tittleWithBackArrow
 
@@ -71,17 +51,11 @@ fun PwChangeScreen(
 
         Spacer(Modifier.padding(15.dp))
 
-        TextFieldOfPwChange(
-            "새 비밀번호 입력 (문자,숫자 포함 6~20자)",
-            "비밀번호", userModifyViewModel.rememberPW
-        ) {
+        TextFieldOfPwChange( userModifyViewModel.rememberPW) {
             userModifyViewModel.updateRememberPW(it)
         }
         Spacer(Modifier.padding(10.dp))
-        TextFieldOfPwCheck(
-            "",
-            "비밀번호 확인",
-            userModifyViewModel.rememberPWCheck,
+        TextFieldOfPwCheck( userModifyViewModel.rememberPWCheck,
             userModifyViewModel.rememberPwEqualOrNot.value
         ) {
             userModifyViewModel.updateRememberPWCheck(it)
@@ -96,172 +70,62 @@ fun PwChangeScreen(
 
 @Composable
 fun TextFieldOfPwChange(
-    inWord: String,
-    name: String,
     pw: State<String>,
     onRememberPw: (String) -> Unit
 ) {
-    val screenWidthInDp = (GetScreenWidthInDp() - 326) / 2
-    val passwordVisible = remember { mutableStateOf(false) }
+    val redBool = pw.value.length < 6 || pw.value.length > 20
     Row(
-        Modifier, //horizontalArrangement = Arrangement.Start
+        Modifier
     ) {
         Spacer(Modifier.weight(0.17f))
-        Text(
-            text = name,
-            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-            fontSize = 16.sp,
-            lineHeight = 18.sp,
-            fontWeight = FontWeight(400),
-            color = Color(0xFF000000),
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .height(19.dp)
-                .weight(0.24f)
 
-        )
-        if (pw.value.length < 6 || pw.value.length > 20) {
-            RedWarning(
-                "* 6자 이상 20자 이내로 작성해 주세요",
-                modifier = Modifier
-                    .height(18.dp)
-                    .weight(0.8f)
-                    .padding(top = 3.dp)
+        TextfiledTitle("비밀번호","* 6자 이상 20자 이내로 작성해 주세요",
+            Modifier
+            .height(19.dp)
+            .weight(0.24f),
+            redBool,"                       ",
+            Modifier
+            .height(18.dp)
+            .weight(0.8f)
+            .padding(top = 3.dp)
             )
-        } else {
-            RedWarning(
-                "                       ",
-                Modifier
-                    .height(18.dp)
-                    .weight(0.8f)
-                    .padding(top = 3.dp)
-            )
-        }
+
         Spacer(Modifier.weight(0.3f))
 
     }
 
     Spacer(Modifier.padding(5.dp))
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(57.dp)
-            //.width(360.dp)
-            .padding(start = screenWidthInDp.dp, top = 3.dp, end = screenWidthInDp.dp)
-            .border(
-                width = 1.dp, color = Color(0xFFBFBFBF),
-                shape = RoundedCornerShape(8.dp)
-            )
-    ) {
-        TextField(
-            value = pw.value,
-            onValueChange = onRememberPw,
-            visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
-            placeholder = { Text(text = inWord, color = Color.Gray, fontSize = 14.sp) },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.White,
-                focusedIndicatorColor = Color.Transparent, // 포커스되었을 때의 밑줄 색상
-                unfocusedIndicatorColor = Color.Transparent, // 포커스가 해제되었을 때의 밑줄 색상
-                disabledIndicatorColor = Color.Transparent // 비활성화되었을 때의 밑줄 색상
-            ),
-            modifier = Modifier
-                .weight(0.8f)
-
-        )
-        IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
-            Icon(
-                painter = painterResource(id = if (passwordVisible.value) R.drawable.visibility else R.drawable.visibility_off),
-                contentDescription = null // decorative element
-            )
-        }
-        Spacer(Modifier.weight(0.02f))
-    }
+    VisibleText(pw,onRememberPw,"새 비밀번호 입력 (문자,숫자 포함 6~20자)",1f,0.00005f)
 }
 
 
 @Composable
 fun TextFieldOfPwCheck(
-    inWord: String,
-    name: String,
     pwcheck: State<String>,
     equal: Boolean,
     onRememberPwCheck: (String) -> Unit
 ) {
-    val screenWidthInDp = (GetScreenWidthInDp() - 326) / 2
-    val passwordVisible = remember { mutableStateOf(false) }
     Row(
-        Modifier, //horizontalArrangement = Arrangement.Start
+        Modifier
     ) {
         Spacer(Modifier.weight(0.19f))
-        Text(
-            text = name,
-            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-            fontSize = 16.sp,
-            lineHeight = 18.sp,
-            fontWeight = FontWeight(400),
-            color = Color(0xFF000000),
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .height(19.dp)
-                .weight(0.4f)
-        )
-        if (!equal) {
-            RedWarning(
-                "* 비밀번호가 일치하지 않습니다",
-                modifier = Modifier
-                    .height(18.dp)
-                    .weight(0.8f)
-                    .padding(top = 3.dp)
-            )
-        } else {
-            RedWarning(
-                "                 ",
-                modifier = Modifier
-                    .height(18.dp)
-                    .weight(0.8f)
-                    .padding(top = 3.dp)
-            )
-        }
+        TextfiledTitle("비밀번호 확인","* 비밀번호가 일치하지 않습니다",
+            Modifier
+            .height(19.dp)
+            .weight(0.4f),!equal,"                 ",
+            Modifier
+            .height(18.dp)
+            .weight(0.8f)
+            .padding(top = 3.dp))
+
         Spacer(Modifier.weight(0.3f))
     }
 
     Spacer(Modifier.padding(5.dp))
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(55.dp)
-            .padding(start = screenWidthInDp.dp, end = screenWidthInDp.dp)
-            .border(
-                width = 1.dp, color = Color(0xFFBFBFBF),
-                shape = RoundedCornerShape(8.dp)
-            )
-    ) {
-        TextField(
-            value = pwcheck.value,
-            onValueChange = onRememberPwCheck,
-            visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
-            placeholder = { Text(text = inWord, color = Color.Gray, fontSize = 14.sp) },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.White,
-                focusedIndicatorColor = Color.Transparent, // 포커스되었을 때의 밑줄 색상
-                unfocusedIndicatorColor = Color.Transparent, // 포커스가 해제되었을 때의 밑줄 색상
-                disabledIndicatorColor = Color.Transparent // 비활성화되었을 때의 밑줄 색상
-            ),
-            modifier = Modifier
-                .weight(1f)
-        )
-        IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
-            Icon(
-                painter = painterResource(id = if (passwordVisible.value) R.drawable.visibility else R.drawable.visibility_off),
-                contentDescription = null // decorative element
-            )
-        }
-        Spacer(Modifier.weight(0.02f))
-    }
+    VisibleText(pwcheck,onRememberPwCheck,"",1f,0.0005f)
+
 }
 
 @Composable
